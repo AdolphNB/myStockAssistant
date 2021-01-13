15effe000e8395c67c6e907f3f88eefdf6b47ddc
abel <abel@mail.com>
2021-01-13 18:35:18 +0800

    update

---
+++ task_pyqt/task_pyqt.py
@@ -0,0 +1,140 @@
+import sys
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QListWidget, QFrame, QLabel,QLineEdit, QGridLayout)
+from PyQt5.QtGui import QColor,QFont
+from PyQt5.QtCore import QTimer, QThread
+
+
+class MainDaemonThread(QThread):
+    def __init__(self):
+        super().__init__()
+        self.srcData = 0
+        self.dataOpt = 0
+
+    def run(self):
+        while True:
+            if self.srcData <= self.dataOpt:
+                print("Thread>>>>")
+            time.sleep(1)
+
+    def compareValue(srcData, datOpt):
+        self.srcData = srcData
+        self.dataOpt = datOpt
+
+
+
+class MainWindow(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.initLabel()
+        self.initButton()
+        self.initQLineEdit()
+        #self.mainDTh = MainDaemonThread()
+        #self.mainDTh.start()
+        self.initQListWidget_wechatList()
+        self.initQListWidget_customerList()
+        self.initQListWidget_strategyList()
+        self.initQListWidget_stockList()
+        self.setWindowTitle('myStockAssistant')
+        self.setGeometry(100, 100, 1500, 800)
+        self.show()
+
+    def initLabel(self):
+        self.label = QLabel(self)
+        self.label.setText("000000")
+        self.label.move(140, 25)
+        self.label.setFont(QFont("0", 20, QFont.Bold))
+
+    def initQListWidget_wechatList(self):
+        listWidget  = QListWidget(self)
+        listWidget.move(40, 150)
+        listWidget.resize(160, 600)
+        listWidget.addItem("Item 1")
+        listWidget.addItem("Item 2")
+        listWidget.addItem("Item 3")
+        listWidget.addItem("Item 4")
+        listWidget.itemClicked.connect(self.signalListWidget_clicked)  # 绑定点击事件
+
+    def initQListWidget_customerList(self):
+        listWidget  = QListWidget(self)
+        listWidget.move(230, 150)
+        listWidget.resize(160, 600)
+        listWidget.addItem("Item 1")
+        listWidget.addItem("Item 2")
+        listWidget.addItem("Item 3")
+        listWidget.addItem("Item 4")
+        listWidget.itemClicked.connect(self.signalListWidget_clicked)  # 绑定点击事件
+
+    def initQListWidget_stockList(self):
+        listWidget  = QListWidget(self)
+        listWidget.move(500, 150)
+        listWidget.resize(160, 600)
+        listWidget.addItem("Item 1")
+        listWidget.addItem("Item 2")
+        listWidget.addItem("Item 3")
+        listWidget.addItem("Item 4")
+        listWidget.itemClicked.connect(self.signalListWidget_clicked)  # 绑定点击事件
+
+    def initQListWidget_strategyList(self):
+        listWidget  = QListWidget(self)
+        listWidget.move(730, 150)
+        listWidget.resize(160, 600)
+        listWidget.addItem("Item 1")
+        listWidget.addItem("Item 2")
+        listWidget.addItem("Item 3")
+        listWidget.addItem("Item 4")
+        listWidget.itemClicked.connect(self.signalListWidget_clicked)  # 绑定点击事件
+
+    def initQLineEdit(self):
+        self.lineEdit = QLineEdit(self)
+        self.lineEdit.move(10, 100)
+        self.lineEdit.setText('600550')
+
+    # this button is a flag that start/stop stock-data update by press or release it.
+    def initButton(self):
+        self.qbtn = QPushButton("START", self)
+        self.status = False
+        self.qbtn.clicked.connect(self.signalBtn_START_STOP)
+        self.qbtn.move(10, 30)
+
+    # this method is a SIGNAL-response callback method, initialize method see initButton()
+    def signalBtn_START_STOP(self):
+        if self.status == False:
+            self.status = True
+            self.stock.setPostStatus(True)
+            self.qbtn.setText("STOP")
+            self.stock.resetStockNumber(self.lineEdit.text())
+            # print("START")
+        else:
+            self.status = False
+            self.stock.setPostStatus(False)
+            self.qbtn.setText("START")
+            # print("STOP")
+
+    def signalListWidget_clicked(self, item):
+        print("qlistwidget click")
+
+    def operate(self):
+        buf = self.stock.getCurrentPrice()
+        self.label.setText(buf)
+
+    def closeEvent(self, event):
+        print("closeEvent.......")
+        print("exit windows.")
+
+    def mousePressEvent(self, event):
+        print("mouse press event....")
+
+'''
+Main function, PyQt5 come ture.
+'''
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    sys.exit(app.exec_())
+
+
+
